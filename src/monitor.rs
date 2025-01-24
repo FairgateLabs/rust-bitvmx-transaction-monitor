1cdaadf34aab6fe05a3cd862b6390c1a18d81c80
Nacho <69277516+Ignacio-87@users.noreply.github.com>
2025-01-24 11:51:28 -0300

    Improve peg in transaction acceptance (#14)
    Co-authored-by: Pedro Prete <pmprete@Pedros-MacBook-Pro.local>

--- src/monitor.rs
+++ src/monitor.rs
@@ -7,6 +7,7 @@ use crate::types::{
     AddressStatus, BitvmxInstance, BlockInfo, InstanceData, InstanceId, TransactionStatus,
     TransactionStore,
 };
+use bitcoin::hex::DisplayHex;
 use bitcoin::script::Instruction;
 use bitcoin::{Address, Network, Script, Transaction, Txid};
 use bitcoin_indexer::indexer::IndexerApi;
@@ -18,6 +19,7 @@ use bitvmx_bitcoin_rpc::types::{BlockHeight, FullBlock};
 use mockall::automock;
 use storage_backend::storage::Storage;
 use tracing::info;
+
 pub struct Monitor<I, B>
 where
     I: IndexerApi,
@@ -489,15 +491,16 @@ where
         if first_part != "RSK_PEGIN" {
             return false;
         }
-
+       
         // Second part should be a number for the packet number
-        let second_part = String::from_utf8_lossy(&data[1]);
-        if second_part.parse::<u64>().is_err() {
+        if data[1].len() != 8 {
             return false;
         }
 
+        //TODO: validate packet number
+
         // Third part should be RSK address
-        let third_part = String::from_utf8_lossy(&data[2]);
+        let third_part = data[2].as_hex().to_string();
         if !Self::is_valid_rsk_address(&third_part) {
             return false;
         }
@@ -508,13 +511,12 @@ where
             return false;
         }
 
-        true
+        return true;
     }
 
     pub fn is_valid_rsk_address(address: &str) -> bool {
-        address.starts_with("0x")
-            && address.len() == 42
-            && address[2..].chars().all(|c| c.is_ascii_hexdigit())
+            address.len() == 40 &&
+            address.chars().all(|c| c.is_ascii_hexdigit())
     }
 
     /// Validates if a transaction is a valid peg-in transaction by checking:
--- tests/monitor_address_test.rs
+++ tests/monitor_address_test.rs
@@ -1,13 +1,9 @@
 use bitcoin::{
-    absolute::LockTime,
-    key::{rand::thread_rng, Secp256k1},
-    opcodes::all::OP_RETURN,
-    script::Builder,
-    secp256k1::{PublicKey, SecretKey},
-    transaction::Version,
+    absolute::LockTime, hex::{ DisplayHex, FromHex}, key::{rand::thread_rng, Secp256k1}, opcodes::all::OP_RETURN, script::Builder, secp256k1::{PublicKey, SecretKey}, transaction::Version
 };
 use bitcoin::{Address, Amount, Network, Transaction, TxOut};
 use bitcoin_indexer::indexer::MockIndexerApi;
+use bitcoincore_rpc::RawTx;
 use bitvmx_transaction_monitor::{monitor::Monitor, store::MockMonitorStore};
 
 #[test]
@@ -37,7 +33,9 @@ fn test_pegin_address_detection1() -> Result<(), anyhow::Error> {
         script_pubkey: committee_n.script_pubkey(),
     };
 
-    let rootstock_address = b"0x7ac5496aee77c1ba1f0854206a26dda82a81d6d8";
+    let packet_number: u64 = 0;
+    let mut rootstock_address = [0u8; 20];
+    rootstock_address.copy_from_slice(Vec::from_hex("7ac5496aee77c1ba1f0854206a26dda82a81d6d8").unwrap().as_slice());
 
     let reimbursement_addr_data = reimbursement_addr.to_string();
     let mut reimbursement_addr_bytes = [0u8; 62];
@@ -50,7 +48,7 @@ fn test_pegin_address_detection1() -> Result<(), anyhow::Error> {
         script_pubkey: Builder::new()
             .push_opcode(OP_RETURN)
             .push_slice(b"RSK_PEGIN")
-            .push_slice(b"1") // packet_number
+            .push_slice(packet_number.to_be_bytes()) // packet_number
             .push_slice(rootstock_address)
             .push_slice(reimbursement_addr_bytes)
             .into_script(),
@@ -61,8 +59,14 @@ fn test_pegin_address_detection1() -> Result<(), anyhow::Error> {
         version: Version::TWO,
         lock_time: LockTime::ZERO,
         input: vec![], // Inputs would be added by the user
-        output: vec![taproot_output, op_return_output],
+        output: vec![taproot_output.clone(), op_return_output.clone()],
     };
+
+    println!("=======PegInTx raw: {}", pegin_tx.raw_hex());
+    println!("======= txId: {}", pegin_tx.compute_txid());
+    println!("======= wTxId: {}", pegin_tx.compute_wtxid());
+    println!("======= taproot_output: amount:{} size:{} script_pubkey:{}", taproot_output.value, taproot_output.size(), taproot_output.script_pubkey.as_bytes().to_hex_string(bitcoin::hex::Case::Lower));
+    println!("======= op_return_output: amount:{} size:{} script_pubkey:{}, script_pubkey_hex:{}", op_return_output.value, op_return_output.size(), op_return_output.script_pubkey, op_return_output.script_pubkey.as_bytes().to_hex_string(bitcoin::hex::Case::Lower));
 
     // Create a mock monitor to test address detection
     let mock_indexer = MockIndexerApi::new();
@@ -70,7 +74,6 @@ fn test_pegin_address_detection1() -> Result<(), anyhow::Error> {
     mock_store.expect_set_current_block_height().returning(|_| Ok(()));
     
     let monitor = Monitor::new(mock_indexer, mock_store, None, 6)?;
-
 
     // Validate that the committee address (N) is detected
     assert!(monitor.is_a_pegin_tx(committee_n.clone(), &pegin_tx));
